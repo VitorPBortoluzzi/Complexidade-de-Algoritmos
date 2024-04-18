319c1ca57b05c85c11f2c6ef83b3f52e6ae02847
VPB <vitorpossebontra@gmail.com>
2024-04-18 09:28:07 -0300

    A16

--- Anotações/Aula 16/fibonacci.c
+++ Anotações/Aula 16/fibonacci.c
@@ -13,7 +13,21 @@ int fibonacci(int n) {
   } 
   return fibonacci(n-1) + fibonacci(n-2);
 }
-
+/*
+int fibonacci(int n) {
+  int x;
+  
+  if (n == 1) {
+    return(1);
+  }
+  
+  if (n == 2) {
+    return(1);
+  }
+  
+  x = fibonacci(n-1) + fibonacci(n-2);
+  return(x);
+}*/
 // Funcao principal.
 
 int main() {
