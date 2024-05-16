18c5082555827ee82a2526ab76e7d9d055cb244b
VPB <vitorpossebontra@gmail.com>
2024-05-16 08:42:35 -0300

    A

---
+++ Anotações/Aula 16-05/a.c
@@ -0,0 +1,14 @@
+
+public void terceiro (int controle) {
+int x=0;
+do {
+    System.out.println((controle-x)%2); 
+    for(int y=0; (controle-x)>0;y++) {
+        System.out.println((controle-y)%2);
+        }
+    }
+while (x<controle);
+}
+
+N+1+[2+For[2N+4]]
+3N+7
